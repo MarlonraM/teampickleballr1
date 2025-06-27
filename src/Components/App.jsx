02806505fb170fd6bba3ad8af796f7ac9fcb3b48
MarlonraM <marlon.ramirez.e@gmail.com>
2025-06-26 23:09:44 -0400

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import './App.css';
+import MatchList from './components/MatchList'; // <--- This line is the culprit
 import TournamentList from './components/TournamentList';
 import PlayerList from './components/PlayerList'; // <-- ¿Está este import?
 import.meta.env.VITE_API_URL
