74712d24cfefffbfcb1a0e296d83bb42513dbb71
MarlonraM <marlon.ramirez.e@gmail.com>
2025-06-26 23:54:53 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // <--- ¡Esta línea es CRÍTICA!
+                                   // Asegúrate de que cubre todos tus componentes React
   ],
   theme: {
     extend: {},
