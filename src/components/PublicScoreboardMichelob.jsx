72b13bdb612506cb6f2ffd8e3676845daddb26c1
MarlonraM <marlon.ramirez.e@gmail.com>
2025-06-29 13:29:04 -0400

    Update PublicScoreboardMichelob.jsx

--- src/components/PublicScoreboardMichelob.jsx
+++ src/components/PublicScoreboardMichelob.jsx
@@ -231,7 +231,7 @@ function PublicScoreboardMichelob() {
         <div style={styles.pageWrapper}>
             <div style={styles.container}>
                 <header style={styles.header}>
-                    <img src=logoblanco.png" alt="Michelob Ultra" style={styles.logo} />
+                    <img src="/logoblanco.png" alt="Michelob Ultra" style={styles.logo} />
                     <div style={styles.hamburgerIcon}>
                         <div style={styles.hamburgerLine}></div>
                         <div style={styles.hamburgerLine}></div>
