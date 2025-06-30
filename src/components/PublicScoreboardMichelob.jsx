e8f505a31fdf4a3e236073e5eae02c8c0579e4b2
MarlonraM <marlon.ramirez.e@gmail.com>
2025-06-30 00:09:33 -0400

    Update PublicScoreboardMichelob.jsx

--- src/components/PublicScoreboardMichelob.jsx
+++ src/components/PublicScoreboardMichelob.jsx
@@ -65,7 +65,7 @@ function PublicScoreboardMichelob() {
     const styles = {
         pageWrapper: { backgroundColor: '#051638', padding: '8px', minHeight: '100vh' },
         container: { fontFamily: "'Inter', sans-serif, sans-serif", backgroundColor: '#FFFFFF', color: '#051638', minHeight: 'calc(100vh - 16px)', borderRadius: '8px', position: 'relative', paddingTop: '80px' },
-        header: { position: 'fixed', top: '8px', left: '8px', right: '8px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 1.5rem', backgroundColor: '#051638', color: 'white', zIndex: 100, borderTopLeftRadius: '8px', borderTopRightRadius: '8px' },
+        header: { position: 'fixed', top: '0px', left: '0px', right: '0px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 1.5rem', backgroundColor: '#051638', color: 'white', zIndex: 100},
         logo: { height: '24px' },
         hamburgerIcon: { width: '24px', height: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', cursor: 'pointer' },
         hamburgerLine: { width: '100%', height: '3px', backgroundColor: '#E51937', borderRadius: '2px' },
@@ -272,7 +272,7 @@ function PublicScoreboardMichelob() {
   )}
 </main>
 
-                <footer style={styles.footer}>
+                <footer style={styles.footer tex; justify-content: center}>
                     El consumo excesivo de alcohol es perjudicial para la salud ley 42-01.
                 </footer>
             </div>
