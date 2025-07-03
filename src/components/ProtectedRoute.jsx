9654ee83f1fae4d589ba003cf6e97b90bf6b012a
MarlonraM <marlon.ramirez.e@gmail.com>
2025-07-03 13:41:56 -0400

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-
 // 2. Crea un nuevo archivo: src/components/ProtectedRoute.jsx
 
 import React from 'react';
@@ -7,8 +6,10 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
     const token = localStorage.getItem('token');
     if (!token) {
+        // Si no hay token, redirige al usuario a la página de login
         return <Navigate to="/login" replace />;
     }
+    // Si hay un token, muestra el componente protegido
     return children;
 };
 
