9fee9e3613c12f2b0c2a5eabc952a9a65ff86f1c
MarlonraM <marlon.ramirez.e@gmail.com>
2025-07-04 21:04:41 -0400

    Update HorariosTentativosJuegos.jsx

--- src/components/HorariosTentativosJuegos.jsx
+++ src/components/HorariosTentativosJuegos.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from "react";
 import { Clock } from "lucide-react";
-import {statictournaments, staticcourts, statictmatches} from "../data/staticData";
+import { statictournaments, staticcourts, staticmatches } from "../data/staticData";
 
 const HorariosPage = () => {
   // Estados de búsqueda y fecha
